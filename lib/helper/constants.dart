799389c2d2dbd5aae2d60fcdfdde8066b5dddf43
rudolf <rudolfpetrusic2016@outlook.com>
2023-12-05 22:23:02 +0100

    Added enums to create the app's constant values.

---
+++ lib/helper/constants.dart
@@ -0,0 +1,39 @@
+enum DatabaseCollections {
+  users,
+  usersData,
+  reviews,
+}
+
+enum MenuItems{
+  add,
+  signout,
+}
+
+enum MenuItemsPhoto{
+  camera,
+  gallery,
+
+}
+
+enum ReviewMode{
+  add,
+  edit,
+  readOnly,
+}
+
+enum Affordability{
+  $,
+  $$,
+  $$$,
+  $$$$,
+}
+
+
+// ResponsiveSizes.mobile.value ->
+enum ResponsiveSizes{
+  mobile(600.0),
+  webDesktopTablet(601.0);
+
+  const ResponsiveSizes(this.value);
+  final double value;
+}
